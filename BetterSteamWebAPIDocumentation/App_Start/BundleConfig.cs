b2b0b84da161af5efdcb586d06e14f5068e53d0d
babelshift <justin.d.skiles@gmail.com>
2015-11-20 11:46:44 -0500

    Ignore some new folders. Update some packages. Show Steam Web API documentation at the home/index based on the results of ISteamWebAPIUtil.

--- BetterSteamWebAPIDocumentation/App_Start/BundleConfig.cs
+++ BetterSteamWebAPIDocumentation/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace BetterSteamWebAPIDocumentation
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
--- BetterSteamWebAPIDocumentation/Controllers/HomeController.cs
+++ BetterSteamWebAPIDocumentation/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SteamWebAPI2;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,13 @@ namespace BetterSteamWebAPIDocumentation.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            string steamWebApiKey = ConfigurationManager.AppSettings["steamWebApiKey"].ToString();
+            SteamWebSession session = new SteamWebSession(steamWebApiKey);
+            var interfaces = await session.GetSupportedAPIListAsync();
+
+            return View(interfaces);
         }
     }
 }
