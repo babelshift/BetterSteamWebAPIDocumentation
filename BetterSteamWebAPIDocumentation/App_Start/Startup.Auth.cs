668bdfa2fa4338d67b3db3c78e7f6d440b861de6
babelshift <justin.d.skiles@gmail.com>
2015-11-20 09:53:16 -0500

    Initial commit.

---
+++ BetterSteamWebAPIDocumentation/App_Start/Startup.Auth.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace BetterSteamWebAPIDocumentation
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+        }
+    }
+}
---
+++ BetterSteamWebAPIDocumentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BetterSteamWebAPIDocumentation.Startup))]
+namespace BetterSteamWebAPIDocumentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
